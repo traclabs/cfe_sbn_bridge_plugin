e4102dc87dfd9bd2a416ba5f81bb8adec3153d9f
Stephen Hart <swhart@traclabs.com>
2022-08-15 15:20:44 -0500

    added more params

--- cfe_sbn_plugin/cfe_sbn_plugin/cfe_sbn_plugin.py
+++ cfe_sbn_plugin/cfe_sbn_plugin/cfe_sbn_plugin.py
@@ -2,6 +2,7 @@
 
 from fsw_ros2_bridge.fsw_plugin_interface import FSWPluginInterface
 
+import os
 # from fsw_ros2_bridge.telem_info import TelemInfo
 # from fsw_ros2_bridge.command_info import CommandInfo
 
@@ -27,61 +28,49 @@ class FSWPlugin(FSWPluginInterface):
 
         # self._routing_service = None
 
-        self._node.declare_parameter('plugin_params.cfs_root', '/home/swhart/code/cFS')
+        self._node.declare_parameter('plugin_params.cfs_root', '~/code/cFS')
         self._cfs_root = self._node.get_parameter('plugin_params.cfs_root').get_parameter_value().\
             string_value
-
+        if '~' in self._cfs_root:
+            self._cfs_root = os.path.expanduser(self._cfs_root)
         self._node.get_logger().info("  using cfs_root: " + self._cfs_root)
 
-        self._root_dir = self._cfs_root + "/tools/cFS-GroundSystem/Subsystems"
-        # self._tlm_def_file = f"{self._root_dir}/tlmGUI/telemetry-pages.txt"
-        # self._cmd_def_file = f"{self._root_dir}/cmdGui/command-pages.txt"
-
-        self._msg_pkg = "cfs_groundsystem_msgs"
+        self._node.declare_parameter('plugin_params.msg_pkg', 'cfe_msgs')
+        self._msg_pkg = self._node.get_parameter('plugin_params.msg_pkg').get_parameter_value().\
+            string_value
+        self._node.get_logger().info("  using msg_pkg: " + self._msg_pkg)
         self._cfs_msgs_dir = get_package_share_directory(self._msg_pkg)
 
-        # self._telem_pages_info = TelemPagesInfo(self._tlm_def_file, self._cfs_msgs_dir)
-        # self._cmd_pages_info = CmdPagesInfo(self._cmd_def_file, self._cfs_msgs_dir)
+        self._node.declare_parameter('plugin_params.udp_receive_port', 1234)
+        self._udp_receive_port = self._node.get_parameter('plugin_params.udp_receive_port').get_parameter_value().\
+            integer_value
+        self._node.get_logger().info("  using udp_receive_port: " + str(self._udp_receive_port))
 
+        self._node.declare_parameter('plugin_params.udp_send_port', 1235)
+        self._udp_send_port = self._node.get_parameter('plugin_params.udp_send_port').get_parameter_value().\
+            integer_value
+        self._node.get_logger().info("  using udp_send_port: " + str(self._udp_send_port))
+
+        self._node.declare_parameter('plugin_params.udp_ip', '127.0.0.1')
+        self._udp_ip = self._node.get_parameter('plugin_params.udp_ip').get_parameter_value().\
+            string_value
+        self._node.get_logger().info("  using udp_ip: " + self._udp_ip)
+
+
+        # these lists will hold information about the message structures and MIDs once we are parsing
+        # the info from the param file and the juicer sql databases
         self._telem_info = []
         self._command_info = []
 
         self._recv_map = {}
-        # for i in range(self._telem_pages_info.get_telem_map_size()):
-        #     key = self._telem_pages_info.get_telem_desc(i)
-        #     # if key != "ES HK Tlm": continue
-        #     def_file = self._telem_pages_info.get_telem_def_file(i)
-        #     msg_type = self._telem_pages_info.get_ros_msg_name(i)
-        #     if def_file == "null":
-        #         continue
-        #     self._recv_map[key] = CFSTelemReciever(self._telem_pages_info, key,
-        #                                            self._telem_pages_info.get_telem_app_id(i),
-        #                                            f"{self._root_dir}/tlmGUI/", def_file)
-        #     topic_name = Path(def_file).stem.replace("-", "_")
-        #     t = TelemInfo(key, msg_type, topic_name)
-        #     self._telem_info.append(t)
 
-        # self._broad_map = {}
-        # for i in range(self._cmd_pages_info.get_cmd_map_size()):
-        #     key = self._cmd_pages_info.get_cmd_desc(i)
-        #     def_file = self._cmd_pages_info.get_cmd_def_file(i)
-        #     msg_type = self._cmd_pages_info.get_ros_msg_name(i)
-        #     if def_file == "":
-        #         continue
-        #     topic_name = Path(def_file).stem.replace("-", "_").lower()
-        #     topic_name = topic_name.replace("__", "_")
-        #     self._broad_map[key] = CFSCommandBroadcaster(self._node, self._cmd_pages_info, key,
-        #                                                  self._cmd_pages_info.get_cmd_app_id(i),
-        #                                                  f"{self._root_dir}/cmdGui/",
-        #                                                  def_file, topic_name)
-        #     c = CommandInfo(key, msg_type, topic_name, self._broad_map[key].process_callback)
-        #     self._command_info.append(c)
 
-        # self.init_routing_service()
-
-    # def init_routing_service(self):
-    #     self._routing_service = RoutingService(self._node)
-    #     self._routing_service.start()
+        ###########################################################
+        ###########################################################
+        # initialize/configure connection to SBN application here
+        ###########################################################
+        ###########################################################
+        self._node.get_logger().info("Setting up connection to SBN application!!")
 
     def get_telemetry_message_info(self):
         return self._telem_info
