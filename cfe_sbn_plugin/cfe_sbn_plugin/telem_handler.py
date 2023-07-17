71959907f3e9139944fbb8e4b079348cc608416d
David Edell <david.edell@jhuapl.edu>
2023-07-17 14:47:23 -0400

    SBN Updates to support binary packet passthrough (ie: for CFDP)

--- cfe_sbn_plugin/cfe_sbn_plugin/telem_handler.py
+++ cfe_sbn_plugin/cfe_sbn_plugin/telem_handler.py
@@ -1,6 +1,9 @@
-import juicer_util.juicer_interface
 from struct import unpack
 import importlib
+import sys
+
+import juicer_util.juicer_interface
+from cfe_msgs.msg import BinaryPktPayload  # Default binary packet format
 
 
 class TelemHandler():
@@ -21,11 +24,29 @@ class TelemHandler():
         packet_id = self.get_pkt_id(datagram)
         if packet_id in self._tlm_map:
             ros_name = self._tlm_map[packet_id]
-            # self._node.get_logger().info("Received packet for " + ros_name)
-            MsgType = getattr(importlib.import_module(self._msg_pkg + ".msg"), ros_name)
-            msg = MsgType()
-            setattr(msg, "seq", self.get_seq_count(datagram))
-            self._juicer_interface.parse_packet(datagram, 0, self._tlm_map[packet_id], msg, self._msg_pkg)
+            
+            if not ros_name:
+                # Packet is defined in configuration, but no structure was specified, implying we should use default binary encoding
+                self._node._logger.warn("Received packet for pid without struct name! Handling Under testing: " + str(packet_id))
+
+                #@ Create Generic Binary Packet containing payload
+                # ROS doesn't seem to allow a msg definition of a simple bytes array, so we must split it
+                #  into an array of individual byte objects (byte[])
+                # VERIFY: Is this correct and/or the most efficient way to do this?
+                tmp_data = datagram[16:]
+                msg_data = [i.to_bytes(1, sys.byteorder) for i in tmp_data]
+                msg = BinaryPktPayload(data=msg_data)
+            else:
+                # self._node.get_logger().info("Received packet for " + ros_name)
+                MsgType = getattr(importlib.import_module(self._msg_pkg + ".msg"), ros_name)
+
+                msg = MsgType()
+                self._juicer_interface.parse_packet(datagram, 0, self._tlm_map[packet_id], msg, self._msg_pkg)
+                
+            msg_attrs = dir(msg)
+            if "seq" in msg_attrs:
+                setattr(msg, "seq", self.get_seq_count(datagram))
+            
             key = self._key_map[packet_id]
             return (key, msg)
         else:
